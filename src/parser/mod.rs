2942e35e561390351190ee8555624895c40863f1
Alexander <just.std@gmail.com>
2019-03-19 21:01:59 +0300

    Simple syntax implement

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -3,12 +3,23 @@ pub mod syntax_tree;
 
 #[test]
 fn lang() {
-    let states = lang::StatementsParser::new()
-        .parse("x is Real; x is Unknown; x + y == 0 => y + x == 0;")
-        .unwrap();
-    // assert_eq!(&format!("{:?}", expr), "soso");
+    let test = "x in Real; x is Unknown;";
+    println!("{}", test);
+    let states = lang::StatementsParser::new().parse(test).unwrap();
     for s in states {
         s.visual();
-        // println!("{:?}", s);
+    }
+
+    let test2 = "x*(y+z) == 0 => y+z == 0 || x == 0;";
+    println!("{}", test2);
+    let states2 = lang::StatementsParser::new().parse(test2).unwrap();
+    for s in states2 {
+        s.visual();
+    }
+    let test3 = "-sin(x) == 0 => x == Pi*n && n in Z;";
+    println!("{}", test3);
+    let states3 = lang::StatementsParser::new().parse(test3).unwrap();
+    for s in states3 {
+        s.visual();
     }
 }
