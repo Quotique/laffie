0bd6d37ddc9c49f572a782e9b4f0f1015702ee79
Quotique <just.std@gmail.com>
2023-02-15 23:02:29 +0300

    fix: deploy updated

--- deploy/push.sh
+++ deploy/push.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-DEPLOY_HOST="10.200.200.6"
-DEPLOY_USER="minerva"
+DEPLOY_HOST="10.110.110.1"
+DEPLOY_USER="laffie"
 
 cargo build --release --target aarch64-unknown-linux-gnu;
 
 echo "STOPING SERVICE"
-ssh $DEPLOY_USER@$DEPLOY_HOST systemctl --user stop minerva.service
+ssh $DEPLOY_USER@$DEPLOY_HOST systemctl --user stop laffie.service
 
 echo "COPYING EXECUTION FILE"
 rsync -zav --rsync-path="mkdir -p /home/$DEPLOY_USER/bin && rsync"\
@@ -17,11 +17,11 @@ echo "COPYING SYMBOLS FILE"
 rsync -zav --delete symbols $DEPLOY_USER@$DEPLOY_HOST:~;
 echo "COPYING SYSTEMD FILE"
 rsync -zav --rsync-path="mkdir -p /home/$DEPLOY_USER/.config/systemd/user && rsync"\
-    deploy/minerva.service $DEPLOY_USER@$DEPLOY_HOST:~/.config/systemd/user/;
+    deploy/laffie.service $DEPLOY_USER@$DEPLOY_HOST:~/.config/systemd/user/;
 
 echo "UPDATE TOKEN"
 ssh $DEPLOY_USER@$DEPLOY_HOST ./secret.sh;
 echo "RELOAD DAEMON"
 ssh $DEPLOY_USER@$DEPLOY_HOST systemctl --user daemon-reload;
 echo "STARTING SERVICE"
-ssh $DEPLOY_USER@$DEPLOY_HOST systemctl --user start minerva.service;
+ssh $DEPLOY_USER@$DEPLOY_HOST systemctl --user start laffie.service;
