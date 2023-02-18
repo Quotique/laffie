dcad213c2a5e511756e368aa2585537acdfb5477
Quotique <just.std@gmail.com>
2023-02-18 05:59:12 +0300

    fix: Crash on quadratic equation witn no roots

--- mcore/src/predefine/operations/sqrt.rs
+++ mcore/src/predefine/operations/sqrt.rs
@@ -9,21 +9,23 @@ use crate::statement::{
 pub fn symbol() -> Symbol {
     Symbol::builder()
         .name("sqrt")
-        .with_calculator(Box::new(power))
+        .with_calculator(Box::new(sqrt))
         .build()
         .unwrap()
 }
 
-pub fn power(root: &mut StatementNode) -> bool {
+pub fn sqrt(root: &mut StatementNode) -> bool {
     if !root.data().is_symbol_name("sqrt") {
         return false;
     }
 
-    let mut result = false;
+    if root.degree() != 1 {
+        panic!("'sqrt' is unary operator!");
+    }
 
-    if root.degree() == 1 {
-        let last = root.pop_back().unwrap();
-        if let Term::Number(d) = &last.data() {
+    let last = root.pop_back().unwrap();
+    if let Term::Number(d) = &last.data() {
+        if d >= &Decimal::from(0) {
             let (mut m, mut e) = d.as_bigint_and_exponent();
             if e.is_odd() {
                 m *= 10;
@@ -32,16 +34,11 @@ pub fn power(root: &mut StatementNode) -> bool {
             let r = m.sqrt();
             if m == &r * &r {
                 *root.data_mut() = Term::Number(Decimal::new(r, e / 2));
-                result = true;
-            } else {
-                root.push_back(last);
+                return true;
             }
-        } else {
-            root.push_back(last);
         }
-    } else {
-        panic!("'sqrt' is unary operator!");
     }
+    root.push_back(last);
 
-    result
+    false
 }
